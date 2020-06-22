0287bbb9413015ec30381ede2802a5c0f967006c
Paul M. Jessup <pauljessup@A-021973.local>
2020-06-22 16:18:21 -0400

    Refactored the entire functionality and function names so it makes more sense programmatically. Factory classes make more sense now, in the way they operate. Need to update readme.md next

--- yarnparse.lua
+++ yarnparse.lua
@@ -24,99 +24,134 @@ function string:extract(open, close)
     return b[1]
 end
 
+
+
 yarnparse={
     json=require("json.json")
 } 
 
-yarnparse.load_file=function(self, filename)
-    self.nodes={}
-    self.hashmap={}
-    self.file=filename
-
+yarnparse.load=function(self, filename)
+    
+    local yarn={}
     local parsed, size=love.filesystem.read("string", filename)
-    self.nodes=self.json.decode(parsed)
+    local hashmap={}
+    local nodes=self.json.decode(parsed)
     --now to create a loopup table, for faster loading by title
-    for i,v in ipairs(self.nodes) do
-        self.hashmap[v.title]=i
+    for i,v in ipairs(nodes) do
+        hashmap[v.title]=i
     end
-end
 
-yarnparse.get_choices=function(self, text)
---this parses the choices it uses to connect to other nodes
---and returns it as a list.
-    local c=text:split("%[%[Answer:")
-    local choices={}
-    local b=c[1]
-    for i,v in ipairs(c) do
-        if(i>1) then
-            v=v:split(']]')
-            v=v[1]
-            local nxt=v:split("|")
-            --splits it so there's the text for the choice, and the node it links to.
-            choices[#choices+1]={text=nxt[1], node=nxt[2]}
-        end
-    end
-    return b, choices
-end
-
---returns a list of each line of the body
---which will make it easier to parse commands
-yarnparse.parse_body=function(self, text)
-    local lines=text:split('\n')
     return {
-         text=text,
-         lines=lines,
-         at=0,
-         total=#lines,
-         traverse=function(self)
-            self.at=self.at+1
-            if(self.at>self.total) then return nil end
-            local ret=self.lines[self.at]
-            --check to see if it's a command, if so, do that.
-                if string.match(ret, "<<") then
-                    ret=ret:extract("<<", ">>")
-                    local f=loadstring(ret)
-                    f()
-                    --let the main program know we're running a command.
-                    return ret, true
+        file=filename,
+        nodes=nodes,
+        hashmap=hashmap,
+
+        --get node function--
+        get_node=function(self, node)
+            --if it's not a node id, use the lookup table
+            if(type(node)~="number") then
+                --error if title does not exist in lookup table.
+                if(self.hashmap[node]==nil) then error("Node " .. node .. " does not exist in " .. self.file  .. ".  Please check spelling.") end
+                --if all is kosher, use that lookup table
+                node=self.hashmap[node]
+            end
+        
+            --load the node through the ID
+            local v=self.nodes[node]
+        
+            --grab the body, and any choices it has
+            local body, choices=self:get_choices(v.body)
+            
+            return {
+                    --this just returns a list containing all the info from
+                    --a node. 
+                    id=node,
+                    title=v.title,
+                    tags=v.tags:split(", "),
+                    body=yarnparse:parse_body(v.body),
+                    choices=choices,
+                    colorId=v.colorId
+                }
+        end,
+
+        --get choice function
+        get_choices=function(self, text)
+            --this parses the choices it uses to connect to other nodes
+            --and returns it as a list.
+                local c=text:split("%[%[Answer:")
+                local choices={}
+                local b=c[1]
+                for i,v in ipairs(c) do
+                    if(i>1) then
+                        v=v:split(']]')
+                        v=v[1]
+                        local nxt=v:split("|")
+                        --splits it so there's the text for the choice, and the node it links to.
+                        choices[#choices+1]={text=nxt[1], node=nxt[2]}
+                    end
                 end
-            return ret, false
-         end,
-         done=function(self)
-            if(self.at>self.total) then return true else return false end
-         end
+                return b, choices
+            end,
+
+            --parse body function
+
+            parse_body=function(self, text)
+                local lines=text:split('\n')
+                return {
+                     text=text,
+                     lines=lines,
+                     at=0,
+                     total=#lines,
+                     traverse=function(self)
+                        self.at=self.at+1
+                        if(self.at>self.total) then return nil end
+                        local ret=self.lines[self.at]
+                        --check to see if it's a command, if so, do that.
+                            if string.match(ret, "<<") then
+                                ret=ret:extract("<<", ">>")
+                                local f=loadstring(ret)
+                                f()
+                                --let the main program know we're running a command.
+                                return ret, true
+                            end
+                        return ret, false
+                     end,
+                     done=function(self)
+                        if(self.at>self.total) then return true else return false end
+                     end
+                }
+            end,
+
+            get_node=function(self, node)
+                --if it's not a node id, use the lookup table
+                if(type(node)~="number") then
+                    --error if title does not exist in lookup table.
+                    if(self.hashmap[node]==nil) then error("Node " .. node .. " does not exist in " .. self.file  .. ".  Please check spelling.") end
+                    --if all is kosher, use that lookup table
+                    node=self.hashmap[node]
+                end
+            
+                --load the node through the ID
+                local v=self.nodes[node]
+            
+                --grab the body, and any choices it has
+                local body, choices=self:get_choices(v.body)
+                
+                return {
+                        --this just returns a list containing all the info from
+                        --a node. 
+                        id=node,
+                        title=v.title,
+                        tags=v.tags:split(", "),
+                        body=self:parse_body(v.body),
+                        choices=choices,
+                        colorId=v.colorId
+                    }
+            end,
+            --make a choice function
+            make_choice=function(self, node, choice)
+                return self:get_node(node.choices[choice].node)
+            end
+
     }
 end
-
---traveses through the node choice
-yarnparse.make_choice=function(self, node, choice)
-    return yarnparse.get_node(node.choices[choice].node)
-end
-
-yarnparse.get_node=function(self, node)
-    --if it's not a node id, use the lookup table
-    if(type(node)~="number") then
-        --error if title does not exist in lookup table.
-        if(self.hashmap[node]==nil) then error("Node " .. node .. " does not exist in " .. self.file  .. ".  Please check spelling.") end
-        --if all is kosher, use that lookup table
-        node=self.hashmap[node]
-    end
-
-    --load the node through the ID
-    local v=self.nodes[node]
-
-    --grab the body, and any choices it has
-    local body, choices=self:get_choices(v.body)
-    
-    return {
-            --this just returns a list containing all the info from
-            --a node. 
-            id=node,
-            title=v.title,
-            tags=v.tags:split(", "),
-            body=yarnparse:parse_body(v.body),
-            choices=choices,
-            colorId=v.colorId
-        }
-end
-
